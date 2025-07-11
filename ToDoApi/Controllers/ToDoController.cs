9d486aad2fbd42a0bd382f000ac826654cc7fcf5
Bhavishay saini <90553362+bhavishhay@users.noreply.github.com>
2025-07-11 11:16:26 +0530

    Add project files.

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+using ToDoApi.Models.DTOs;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDo> toDoList = new List<ToDo>();
+        private static int nextId = 1;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ToDo>> GetAll()
+        {
+            return Ok(toDoList);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ToDo> GetById(int id)
+        {
+            var task = toDoList.FirstOrDefault(t => t.Id == id);
+            if (task == null) return NotFound();
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public ActionResult<ToDo> Create([FromBody] ToDoRequest input)
+        {
+
+            var task = new ToDo
+            {
+                Id = nextId++,
+                Title = input.Title,
+                Description = input.Description,
+                Status = false
+            };
+
+            toDoList.Add(task);
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<ToDo> Update(int id, [FromBody] ToDoResponse input)
+        {
+
+            var task = toDoList.FirstOrDefault(t => t.Id == id);
+            if (task == null) return NotFound();
+
+            task.Title = input.Title;
+            task.Description = input.Description;
+            task.Status = input.Status;
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var task = toDoList.FirstOrDefault(t => t.Id == id);
+            if (task == null) return NotFound();
+
+            toDoList.Remove(task);
+            return NoContent();
+        }
+    }
+}
---
+++ ToDoApi/Models/DTOs/ToDoRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApi.Models.DTOs
+{
+    public class ToDoRequest
+    {
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
